5d7d36a4ee20f36ccd0ab9e3eb11f4e175551f1f
shizuku86 <shizuku86710@gmail.com>
2025-08-02 20:06:01 +0900

    Reordered method definitions

--- src/router/init/behaviorInitializeRegister.ts
+++ src/router/init/behaviorInitializeRegister.ts
@@ -1,14 +1,14 @@
 import type { ScriptEventCommandMessageAfterEvent } from "@minecraft/server";
 
 export class BehaviorInitializeRegister {
-    private static registerAddon(message: string): void {
-
-    }
-
     static handleScriptEventReceive(ev: ScriptEventCommandMessageAfterEvent): void {
         const { id, message} = ev;
 
         if (id !== "router:initializeResponse") return;
         this.registerAddon(message);
+    }
+
+    private static registerAddon(message: string): void {
+
     }
 }
--- src/router/init/behaviorInitializeRequest.ts
+++ src/router/init/behaviorInitializeRequest.ts
@@ -2,12 +2,12 @@ import { system, WorldLoadAfterEvent } from "@minecraft/server";
 import { ConsoleManager } from "../../utils/consoleManager";
 
 export class BehaviorInitializeRequest {
+    static handleWorldLoad(ev: WorldLoadAfterEvent): void {
+        this.sendRequest();
+    }
+
     private static sendRequest(): void {
         ConsoleManager.info("Router", "World loaded. Sending core initialization request...");
         system.sendScriptEvent("router:initializeRequest", "");
-    }
-
-    static handleWorldLoad(ev: WorldLoadAfterEvent): void {
-        this.sendRequest();
     }
 }
