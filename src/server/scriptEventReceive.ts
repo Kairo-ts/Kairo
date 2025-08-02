f2c5084843d7580db0415901a2c2faf91a1fae00
shizuku86 <shizuku86710@gmail.com>
2025-08-02 18:14:35 +0900

    delete server/*

--- src/server/scriptEventReceive.ts
+++
@@ -1,5 +0,0 @@
-import { ScriptEventCommandMessageAfterEvent, system } from "@minecraft/server";
-
-system.afterEvents.scriptEventReceive.subscribe((ev: ScriptEventCommandMessageAfterEvent) => {
-    const { id, message } = ev;
-});
