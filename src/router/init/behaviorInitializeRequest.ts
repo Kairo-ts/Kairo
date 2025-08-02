dbc0e97a1d79dfc0ca9edea10e4211226d1d4c20
shizuku86 <shizuku86710@gmail.com>
2025-08-02 18:59:22 +0900

    fixed name core -> router

--- src/router/init/behaviorInitializeRequest.ts
+++ src/router/init/behaviorInitializeRequest.ts
@@ -3,8 +3,8 @@ import { ConsoleManager } from "../../utils/consoleManager";
 
 export class BehaviorInitializeRequest {
     private static sendRequest(): void {
-        ConsoleManager.info("Core", "World loaded. Sending core initialization request...");
-        system.sendScriptEvent("core:initializeRequest", "");
+        ConsoleManager.info("Router", "World loaded. Sending core initialization request...");
+        system.sendScriptEvent("router:initializeRequest", "");
     }
 
     static handleWorldLoad(ev: WorldLoadAfterEvent): void {
