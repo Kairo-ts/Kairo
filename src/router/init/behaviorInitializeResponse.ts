0e4c48dd91b5741f649b8daa465ac6782dd1d9d7
shizuku86 <shizuku86710@gmail.com>
2025-08-02 20:21:46 +0900

    add sendResponse()

--- src/router/init/behaviorInitializeResponse.ts
+++ src/router/init/behaviorInitializeResponse.ts
@@ -1,5 +1,19 @@
+import { system } from "@minecraft/server";
+import { properties } from "../../properties";
+
 export class BehaviorInitializeResponse {
     static sendResponse(): void {
+        system.sendScriptEvent("router:initializeResponse", this.serializeForTransmission());
+    }
 
+    private static serializeForTransmission(): string {
+        return JSON.stringify(
+            {
+                name: properties.header.name,
+                version: properties.header.version,
+                dependencies: properties.dependencies,
+                requiredAddons: properties.requiredAddons
+            }
+        );
     }
 }
