edc795d35b1804fafbc1f15cac15cc2f7aa62368
shizuku86 <shizuku86710@gmail.com>
2025-08-02 17:29:04 +0900

    addon properties

---
+++ src/properties.ts
@@ -0,0 +1,38 @@
+export const properties = {
+    metadata: { 
+        /** 製作者の名前 */
+        authors: [
+            "静空"
+        ]
+    },
+    header: {
+        name: "Werewolf-Core",
+        description: "アドオン間通信の管理を担う、マイクラ人狼の中枢となるアドオンです",
+        min_engine_version: [ 1,21,90 ],
+        version: [ 0,0,1 ],
+        uuid: "45826daa-bf9f-4443-b746-944a0970bfef"
+    },
+    modules: [
+        {
+            type: "script",
+			language: "javascript",
+			entry: "scripts/index.js",
+            version: "header.version",
+            uuid: "1d3bfdf2-7456-435b-bacf-c94c0d7b7c64"
+        }
+    ],
+    dependencies: [
+		{
+			module_name: "@minecraft/server",
+			version: "2.0.0"
+		},
+		{
+			module_name: "@minecraft/server-ui",
+			version: "2.0.0"
+		}
+	],
+    /** 前提アドオン */
+    requiredAddons: [
+
+    ],
+}
