5406d91133b378faf2e65e140167dd361240443e
shizuku86 <shizuku86710@gmail.com>
2025-08-02 18:14:58 +0900

    add initialize()

---
+++ src/core/behaviorManager.ts
@@ -0,0 +1,24 @@
+import type { ScriptEventCommandMessageAfterEvent, WorldLoadAfterEvent } from "@minecraft/server";
+import { system, world } from "@minecraft/server";
+import { BehaviorInitializeRequest } from "./init/behaviorInitializeRequest";
+import { ConsoleManager } from "../utils/consoleManager";
+import { BehaviorInitializeRegister } from "./init/behaviorInitializeRegister";
+
+export class BehaviorManager {
+    private static handleWorldLoad(ev: WorldLoadAfterEvent): void {
+        ConsoleManager.info("Core", "World loaded. Sending core initialization request...");
+        BehaviorInitializeRequest.sendRequest();
+    }
+
+    private static handleScriptEventReceive(ev: ScriptEventCommandMessageAfterEvent): void {
+        const { id, message } = ev;
+
+        if (id !== "core:initializeResponse") return;
+        BehaviorInitializeRegister.registerAddon(message);
+    }
+
+    static initialize() {
+        world.afterEvents.worldLoad.subscribe(this.handleWorldLoad);
+        system.afterEvents.scriptEventReceive.subscribe(this.handleScriptEventReceive);
+    }
+}
