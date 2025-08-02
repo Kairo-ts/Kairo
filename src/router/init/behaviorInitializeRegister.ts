690f69471c4ad463083c96bf1bcf3b3f54545004
shizuku86 <shizuku86710@gmail.com>
2025-08-02 18:59:12 +0900

    add handler

--- src/router/init/behaviorInitializeRegister.ts
+++ src/router/init/behaviorInitializeRegister.ts
@@ -1,5 +1,14 @@
-export class BehaviorInitializeRegister {
-    static registerAddon(message: string): void {
+import type { ScriptEventCommandMessageAfterEvent } from "@minecraft/server";
 
+export class BehaviorInitializeRegister {
+    private static registerAddon(message: string): void {
+
+    }
+
+    static handleScriptEventReceive(ev: ScriptEventCommandMessageAfterEvent): void {
+        const { id, message} = ev;
+
+        if (id !== "router:initializeResponse") return;
+        this.registerAddon(message);
     }
 }
