e7d4a8167e4c12e7638b3f8a45d8a32a3961127f
shizuku86 <shizuku86710@gmail.com>
2025-08-02 19:34:24 +0900

    make handler & forwardRequest()

--- src/router/init/behaviorInitializeReceive.ts
+++ src/router/init/behaviorInitializeReceive.ts
@@ -1,3 +1,15 @@
+import type { ScriptEventCommandMessageAfterEvent } from "@minecraft/server";
+import { BehaviorInitializeResponse } from "./behaviorInitializeResponse";
+
 export class BehaviorInitializeReceive {
-    
+    private static forwardRequest(): void {
+        BehaviorInitializeResponse.sendResponse();
+    }
+
+    static handleScriptEventReceive(ev: ScriptEventCommandMessageAfterEvent): void {
+        const { id, message } = ev;
+
+        if (id !== "router:initializeRequest") return;
+        this.forwardRequest();
+    }
 }
