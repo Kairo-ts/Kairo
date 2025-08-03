690f9ceeb3979955df6e1747f5ff5403ec7f727d
shizuku86 <shizuku86710@gmail.com>
2025-08-03 19:16:13 +0900

    make add()

--- src/router/init/BehaviorInitializePending.ts
+++ src/router/init/BehaviorInitializePending.ts
@@ -1,3 +1,16 @@
+import type { ScriptEventCommandMessageAfterEvent } from "@minecraft/server";
+
 export class BehaviorInitializePending {
-    
+    private static readonly pendingAddons: Set<string> = new Set();
+
+    static handleScriptEventReceive(ev: ScriptEventCommandMessageAfterEvent): void {
+        const { id, message } = ev;
+
+        if (id !== "router:initializeResponse") return;
+        this.add(message);
+    }
+
+    private static add(message: string): void {
+        this.pendingAddons.add(message);
+    }
 }
