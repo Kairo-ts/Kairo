01a0024b627db098db7936bb2cb3fa729477e7ee
shizuku86 <shizuku86710@gmail.com>
2025-08-04 02:37:25 +0900

    make AddonPropertyManager

--- src/router/AddonProperty.ts
+++ src/router/AddonProperty.ts
@@ -9,3 +9,12 @@ export interface AddonProperty {
         [name: string]: number[];
     };
 }
+
+export class AddonPropertyManager {
+    private static readonly charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+    static generateRandomId(length: number = 8): string {
+        const chars = this.charset.split('');
+        return Array.from({ length }, () => chars[Math.floor(Math.random() * chars.length)]).join('');
+    }
+}
