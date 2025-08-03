075c4c7485b2429c088682ed08a1542620bd111c
shizuku86 <shizuku86710@gmail.com>
2025-08-03 23:15:38 +0900

    deleted handleScriptEvent

--- src/router/init/behaviorInitializeRegister.ts
+++ src/router/init/behaviorInitializeRegister.ts
@@ -1,4 +1,3 @@
-import type { ScriptEventCommandMessageAfterEvent } from "@minecraft/server";
 import { ConsoleManager } from "../../utils/consoleManager";
 
 /**
@@ -11,13 +10,6 @@ import { ConsoleManager } from "../../utils/consoleManager";
  * *Currently, only the functionality for receiving responses is implemented.*
  */
 export class BehaviorInitializeRegister {
-    static handleScriptEventReceive(ev: ScriptEventCommandMessageAfterEvent): void {
-        const { id, message} = ev;
-
-        if (id !== "router:initializeResponse") return;
-        this.registerAddon(message);
-    }
-
     private static registerAddon(message: string): void {
         const addonProperties = JSON.parse(message);
 
