0847f0aa4abac999cef04246ea35c72ff188b711
shizuku86 <shizuku86710@gmail.com>
2025-08-04 02:06:31 +0900

    build

--- BP/scripts/index.js
+++ BP/scripts/index.js
@@ -1,10 +1,11 @@
 import { AddonRouter } from "./router/AddonRouter";
 import { BehaviorManager } from "./router/behaviorManager";
 import { BehaviorInitializePending } from "./router/init/behaviorInitializePending";
+import { BehaviorInitializeRegister } from "./router/init/behaviorInitializeRegister";
 async function main() {
     BehaviorManager.initialize();
     AddonRouter.initialize();
     await BehaviorInitializePending.ready;
-    console.log(BehaviorInitializePending.getAll());
+    BehaviorInitializeRegister.registerAddon();
 }
 main();
