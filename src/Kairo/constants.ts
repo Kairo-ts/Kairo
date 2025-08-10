ad211ce5513c8880a99af19d1d80216acaddb35d
shizuku86 <shizuku86710@gmail.com>
2025-08-10 23:22:25 +0900

    define constants

---
+++ src/Kairo/constants.ts
@@ -0,0 +1,6 @@
+export const SCRIPT_EVENT_IDS = {
+    BEHAVIOR_INITIALIZE_REQUEST: "kairo:initializeRequest",
+    BEHAVIOR_INITIALIZE_RESPONSE: "kairo:initializeResponse",
+    UNSUBSCRIBE_INITIALIZE: "kairo:unsubscribeInitialize",
+    REQUEST_RESEED_SESSION_ID: "kairo:reseedSessionId"
+}
