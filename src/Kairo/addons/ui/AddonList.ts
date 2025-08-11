927f7ffcc16855e718e11584ae7c1776f9375bed
shizuku86 <shizuku86710@gmail.com>
2025-08-12 04:22:38 +0900

    make showAddonList(player)

--- src/Kairo/addons/ui/AddonList.ts
+++ src/Kairo/addons/ui/AddonList.ts
@@ -1,8 +1,23 @@
+import { ActionFormData } from "@minecraft/server-ui";
 import type { AddonManager } from "../AddonManager";
+import type { Player } from "@minecraft/server";
 
 export class AddonList {
     private constructor(private readonly addonManager: AddonManager) {}
     public static create(addonManager: AddonManager): AddonList {
         return new AddonList(addonManager);
+    }
+
+    public showAddonList(player: Player): void {
+        const addonsData = this.addonManager.getAddonsData();
+
+        const addonListForm = new ActionFormData();
+        addonListForm.title({"rawtext": [{ translate: "kairo.addonList.title" }]});
+
+        addonsData.forEach((data, name) => {
+            addonListForm.button({"rawtext": [{ text: name }]});
+        });
+
+        addonListForm.show(player);
     }
 }
