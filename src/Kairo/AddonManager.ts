5f51130d54d7b0e7913745f34c10792dabef175e
shizuku86 <shizuku86710@gmail.com>
2025-08-12 01:48:58 +0900

    initAddonData() & registerAddonData()

--- src/Kairo/AddonManager.ts
+++ src/Kairo/AddonManager.ts
@@ -1,4 +1,5 @@
 import type { Kairo } from ".";
+import { VersionManager } from "../utils/versionManager";
 import type { AddonProperty } from "./AddonPropertyManager";
 
 export interface AddonData {
@@ -28,7 +29,23 @@ export class AddonManager {
         return new AddonManager(kairo);
     }
 
-    public initAddonData(name: string, selectedVersion: string, versions: string[]): void {
+    public activateAddons(addons: AddonProperty[]): void {
+        const addonRecords = this.kairo.getAddonRecords();
+
+        Object.entries(addonRecords).forEach(([name, record]) => {
+            this.initAddonData(name, record.selectedVersion, record.versions);
+        });
+
+        addons.forEach(addon => {
+            this.registerAddonData(addon);
+        });
+
+        // latestの場合は最新の安定板を有効化し、
+        // それ以外は選択されたバージョンを
+        // 選択されているものが読み込まれていなければ、latestに変更する
+    }
+
+    private initAddonData(name: string, selectedVersion: string, versions: string[]): void {
         const addonData: AddonData = {
             name,
             selectedVersion,
@@ -42,7 +59,16 @@ export class AddonManager {
         this.addonsData.set(name, addonData);
     }
 
-    public registerAddonData(addon: AddonProperty): void {
-        
+    private registerAddonData(addon: AddonProperty): void {
+        const addonData = this.addonsData.get(addon.name);
+        if (!addonData) return;
+
+        addonData.versions[VersionManager.toVersionString(addon.version)] = {
+            isAvailable: true,
+            sessionId: addon.sessionId,
+            tags: addon.tags,
+            dependencies: addon.dependencies,
+            requiredAddons: addon.requiredAddons
+        };
     }
 }
