5a991491bd480009c5fb596682695e6d1d05d0ce
shizuku86 <shizuku86710@gmail.com>
2025-08-12 00:50:37 +0900

    defined AddonData

--- src/Kairo/AddonManager.ts
+++ src/Kairo/AddonManager.ts
@@ -1,6 +1,28 @@
 import type { Kairo } from ".";
 
+export interface AddonData {
+    name: string;
+    selectedVersion: string;
+    versions: {
+        [version: string]: {
+            isAvailable: boolean;
+            sessionId?: string;
+            tags?: string[];
+            dependencies?: {
+                module_name: string;
+                version: string;
+            }[];
+            requiredAddons?: {
+                [name: string]: number[];
+            };
+        }
+    }
+}
+
 export class AddonManager {
+    private readonly addonsData: Map<string, AddonData> = new Map();
+
+
     private constructor(private readonly kairo: Kairo) {}
     public static create(kairo: Kairo): AddonManager {
         return new AddonManager(kairo);
