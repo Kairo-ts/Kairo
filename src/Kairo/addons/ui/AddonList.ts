8ec30399df5f91a78cf7643fe5373fc42ceeeca7
shizuku86 <shizuku86710@gmail.com>
2025-08-13 21:37:24 +0900

    rename variables

--- src/Kairo/addons/ui/AddonList.ts
+++ src/Kairo/addons/ui/AddonList.ts
@@ -1,4 +1,4 @@
-import { ActionFormData } from "@minecraft/server-ui";
+import { ActionFormData, ModalFormData } from "@minecraft/server-ui";
 import type { AddonManager } from "../AddonManager";
 import type { Player } from "@minecraft/server";
 
@@ -8,16 +8,31 @@ export class AddonList {
         return new AddonList(addonManager);
     }
 
-    public showAddonList(player: Player): void {
-        const addonsData = this.addonManager.getAddonsData();
+    public async showAddonList(player: Player): Promise<void> {
+        const addonsData = Array.from(this.addonManager.getAddonsData());
 
         const addonListForm = new ActionFormData();
         addonListForm.title({"rawtext": [{ translate: "kairo.addonList.title" }]});
 
-        addonsData.forEach((data, name) => {
-            addonListForm.button({"rawtext": [{ text: name }]});
+        addonsData.forEach(([name, data]) => {
+            if (data.isActive) {
+                addonListForm.button(`§l§8${name}§r\n§l§9有効`);
+            }
+            else {
+                addonListForm.button(`§l§8${name}§r\n§l§4無効`);
+            }
         });
 
-        addonListForm.show(player);
+        const { selection, canceled: listFormCanceled } = await addonListForm.show(player);
+        if (listFormCanceled || selection === undefined) return;
+
+        const selectedAddon = addonsData[selection];
+        if (!selectedAddon) return;
+
+        const addonDataForm = new ModalFormData();
+        addonDataForm.title(selectedAddon[0]);
+
+        const { formValues, canceled: dataFormCanceled } = await addonDataForm.show(player);
+        if (dataFormCanceled) return;
     }
 }
