8c866edef7dc577487830c79a1fbac1a0b28e00c
shizuku86 <shizuku86710@gmail.com>
2025-08-23 05:04:13 +0900

    build

--- BP/scripts/kairo/addons/record/AddonRecord.js
+++ BP/scripts/kairo/addons/record/AddonRecord.js
@@ -10,10 +10,18 @@ export class AddonRecord {
     saveAddons(addons) {
         const addonRecords = this.loadAddons();
         addons.forEach(addon => {
-            const { name, version, tags } = addon;
+            const { name, version } = addon;
             const vStr = VersionManager.toVersionString(version);
             if (!addonRecords[name]) {
-                addonRecords[name] = { selectedVersion: "latest version", versions: [] };
+                addonRecords[name] = {
+                    description: ["0.0.0", ""],
+                    selectedVersion: "latest version",
+                    versions: []
+                };
+            }
+            if (VersionManager.compare(addonRecords[name].description[0], vStr) === -1) {
+                addonRecords[name].description[0] = vStr;
+                addonRecords[name].description[1] = addon.description;
             }
             addonRecords[name].versions.push(vStr);
         });
