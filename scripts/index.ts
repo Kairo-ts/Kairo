c0fc1e991100b97a57a0fe35807e35e56975c114
shizuku86 <shizuku86710@gmail.com>
2025-08-23 10:51:57 +0900

    add dynamicProperty all clear method for development use

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,7 +1,16 @@
 import { Player, ScriptEventCommandMessageAfterEvent, system } from "@minecraft/server";
 import { Kairo } from "./Kairo/index";
+import { DynamicPropertyStorage } from "./Kairo/addons/record/DynamicPropertyStorage";
 
 async function main(): Promise<void> {
+    /**
+     * DynamicPropertyをすべてクリアするメソッド (開発用)
+     * アンコメントで使用してください
+     * A method to clear all DynamicProperties (for development use)
+     * Use by uncommenting
+     */
+    // DynamicPropertyStorage.clear();
+
     Kairo.init(); // client
     Kairo.initRouter();
 
