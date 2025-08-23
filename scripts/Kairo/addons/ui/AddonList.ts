9b8214510f2beadfddb058da7fadfd2188eddfda
shizuku86 <shizuku86710@gmail.com>
2025-08-23 13:46:14 +0900

    transfer handlescriptEvent from root index

--- scripts/Kairo/addons/ui/AddonList.ts
+++ scripts/Kairo/addons/ui/AddonList.ts
@@ -1,11 +1,21 @@
 import { ActionFormData, ModalFormData } from "@minecraft/server-ui";
 import type { AddonManager } from "../AddonManager";
-import type { Player } from "@minecraft/server";
+import type { Player, ScriptEventCommandMessageAfterEvent } from "@minecraft/server";
 
 export class AddonList {
     private constructor(private readonly addonManager: AddonManager) {}
     public static create(addonManager: AddonManager): AddonList {
         return new AddonList(addonManager);
+    }
+
+    public handleScriptEvent = (ev: ScriptEventCommandMessageAfterEvent): void => {
+        const { id, message, sourceEntity } = ev;
+
+        if (sourceEntity?.typeId !== "minecraft:player") return;
+        
+        if (id === "kairo:addonList") {
+            this.showAddonList(sourceEntity as Player);
+        }
     }
 
     public async showAddonList(player: Player): Promise<void> {
