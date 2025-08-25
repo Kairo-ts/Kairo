b69423193e735ac6f8b168a1cba939de1099947d
shizuku86 <shizuku86710@gmail.com>
2025-08-26 02:06:08 +0900

    make constants/ folder

---
+++ scripts/constants/scriptevent.ts
@@ -0,0 +1,12 @@
+export const SCRIPT_EVENT_IDS = {
+    BEHAVIOR_INITIALIZE_REQUEST: "kairo:initializeRequest",
+    BEHAVIOR_INITIALIZE_RESPONSE: "kairo:initializeResponse",
+    UNSUBSCRIBE_INITIALIZE: "kairo:unsubscribeInitialize",
+    REQUEST_RESEED_SESSION_ID: "kairo:reseedSessionId",
+    SHOW_ADDON_LIST: "kairo:showAddonList"
+}
+
+export const SCRIPT_EVENT_MESSAGES = {
+    ACTIVE_REQUEST: "active request",
+    DEACTIVE_REQUEST: "deactive request"
+}
