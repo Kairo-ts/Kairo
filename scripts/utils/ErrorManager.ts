86511fa958a034010d7c64da30b64b7158b2a662
shizuku86 <shizuku86710@gmail.com>
2025-08-25 23:04:06 +0900

    make ErrorManager

---
+++ scripts/utils/ErrorManager.ts
@@ -0,0 +1,7 @@
+import type { Player } from "@minecraft/server";
+
+export class ErrorManager {
+    public static async showErrorDetails(player: Player, errorId: string): Promise<void> {
+        
+    }
+}
