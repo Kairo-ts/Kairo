eae5512b5d37aa534f0e55555a2599b48f960f35
shizuku86 <shizuku86710@gmail.com>
2025-09-01 03:44:37 +0900

    added create()

--- scripts/Kairo/addons/router/AddonVersionChanger.ts
+++ scripts/Kairo/addons/router/AddonVersionChanger.ts
@@ -4,6 +4,10 @@ import type { AddonData, AddonManager } from "../AddonManager";
 export class AddonVersionChanger {
     private constructor(private readonly addonManager: AddonManager) {}
 
+    private static create(addonManager: AddonManager): AddonVersionChanger {
+        return new AddonVersionChanger(addonManager);
+    }
+
     public changeAddonVersion(player: Player, addonData: AddonData, version: string): void {
         this.addonManager.changeAddonVersion(player, addonData, version);
     }
