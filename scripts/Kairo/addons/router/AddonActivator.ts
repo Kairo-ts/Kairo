2f3978f4d8003217a96cb7628ec03ee35f04aec0
shizuku86 <shizuku86710@gmail.com>
2025-09-08 12:13:14 +0900

    make activateAddon()

--- scripts/Kairo/addons/router/AddonActivator.ts
+++ scripts/Kairo/addons/router/AddonActivator.ts
@@ -3,6 +3,8 @@ import type { AddonData, AddonManager } from "../AddonManager";
 import { SCRIPT_EVENT_ID_PREFIX, SCRIPT_EVENT_MESSAGES } from "../../../constants/scriptevent";
 import { KAIRO_TRANSLATE_IDS } from "../../../constants/translate";
 import { AddonRequireValidator } from "./AddonRequireValidator";
+import { VERSION_KEYWORDS } from "../../../constants/version_keywords";
+import { VersionManager } from "../../../utils/VersionManager";
 
 export class AddonActivator {
     private readonly addonRequireValidator: AddonRequireValidator;
@@ -15,15 +17,56 @@ export class AddonActivator {
         return new AddonActivator(addonManager);
     }
 
-    public activateAddon(player: Player, addonData: AddonData, version: string): void {
-        world.sendMessage({ translate: KAIRO_TRANSLATE_IDS.ADDON_ACTIVE, with: [addonData.name, version]});
+    public async activateAddon(player: Player, addonData: AddonData, version: string): Promise<void> {
+        const activateAddonIds = await this.addonRequireValidator.validateRequiredAddonsForActivation(player, addonData, version);
+        if (activateAddonIds.length === 0) return;
+
+        const addonsData = this.getAddonsData();
+        for (const id of activateAddonIds) {
+            const data = addonsData.get(id);
+            if (data) {
+                data.isActive = true;
+                if (data.id === addonData.id) data.selectedVersion = version;
+                else data.selectedVersion = VERSION_KEYWORDS.LATEST;
+
+                const newActiveVersion = data.selectedVersion === VERSION_KEYWORDS.LATEST
+                    ? this.addonManager.getLatestPreferStableVersion(data.id)
+                    : data.selectedVersion;
+                if (newActiveVersion === undefined) continue;
+
+                /**
+                 * 大きくするなら、過去バージョンは問答無用に無効化して良い
+                 * 小さくするなら、deactivationの依存も検証する必要がある
+                 */
+                const compare = VersionManager.compare(data.activeVersion, newActiveVersion);
+                if (compare < 0) {
+                    const oldActiveVersionData = data.versions[data.activeVersion];
+                    const oldSessionId = oldActiveVersionData?.sessionId;
+                    if (oldSessionId) this.sendDeactiveRequest(oldSessionId);
+                }
+                else if (compare > 0) {
+                    const deactivateAddonIds = await this.addonRequireValidator.validateRequiredAddonsForDeactivation(player, data, newActiveVersion);
+                    this.deactivateAddons(deactivateAddonIds);
+                }
+
+                data.activeVersion = newActiveVersion;
+                const newActiveVersionData = data.versions[data.activeVersion];
+                const newSessionId = newActiveVersionData?.sessionId;
+                if (newSessionId) this.sendActiveRequest(newSessionId);
+
+                world.sendMessage({ translate: KAIRO_TRANSLATE_IDS.ADDON_ACTIVE, with: [addonData.name, newActiveVersion]});
+            }
+        }
     }
 
     public async deactivateAddon(player: Player, addonData: AddonData): Promise<void> {
         const deactivateAddonIds = await this.addonRequireValidator.validateRequiredAddonsForDeactivation(player, addonData);
+        this.deactivateAddons(deactivateAddonIds);
+    }
 
+    private deactivateAddons(addonIds: string[]): void {
         const addonsData = this.getAddonsData();
-        for (const id of deactivateAddonIds) {
+        for (const id of addonIds) {
             const data = addonsData.get(id);
             if (data) {
                 data.isActive = false;
@@ -39,18 +82,6 @@ export class AddonActivator {
 
     public getAddonsData(): Map<string, AddonData> {
         return this.addonManager.getAddonsData();
-    }
-
-    public changeAddonSettings(addonData: AddonData, version: string, isActive: boolean): void {
-        addonData.selectedVersion = version;
-        addonData.isActive = isActive;
-
-        const activeVersionData = addonData.versions[addonData.activeVersion];
-        const sessionId = activeVersionData?.sessionId;
-        if (!sessionId) return;
-
-        if (addonData.isActive) this.sendActiveRequest(sessionId);
-        else this.sendDeactiveRequest(sessionId);
     }
 
     public sendActiveRequest(sessionId: string): void {
