8ae31f89d2325f2488ad897c31814ddb56731d95
shizuku86 <shizuku86710@gmail.com>
2025-09-08 17:59:51 +0900

    make router system

--- scripts/Kairo/addons/router/AddonRouter.ts
+++ scripts/Kairo/addons/router/AddonRouter.ts
@@ -1,5 +1,6 @@
-import type { ScriptEventCommandMessageAfterEvent } from "@minecraft/server";
+import { system, type ScriptEventCommandMessageAfterEvent } from "@minecraft/server";
 import type { AddonManager } from "../AddonManager";
+import { SCRIPT_EVENT_ID_PREFIX } from "../../../constants/scriptevent";
 
 export class AddonRouter {
     private constructor(private readonly addonManager: AddonManager) {}
@@ -9,6 +10,17 @@ export class AddonRouter {
     }
 
     public handleScriptEvent(ev: ScriptEventCommandMessageAfterEvent): void {
-        console.log("test");
+        const { id, message } = ev;
+        const idSuffix = id.split(":")[1];
+        if (idSuffix === undefined) return;
+
+        const addonData = this.addonManager.getAddonsData().get(idSuffix);
+        if (addonData === undefined) return;
+        if (!addonData.isActive) return;
+
+        const activeVersionData = addonData.versions[addonData.activeVersion];
+        if (!activeVersionData) return;
+
+        system.sendScriptEvent(`${SCRIPT_EVENT_ID_PREFIX.KAIRO}:${activeVersionData.sessionId}`, message);
     }
 }
