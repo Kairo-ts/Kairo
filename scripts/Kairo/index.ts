d809eee70be60e6c9b6e88dc16ffd3da88f54926
shizuku86 <shizuku86710@gmail.com>
2025-09-28 17:49:07 +0900

    make datavault receiver

--- scripts/Kairo/index.ts
+++ scripts/Kairo/index.ts
@@ -74,6 +74,14 @@ export class Kairo {
         system.sendScriptEvent(SCRIPT_EVENT_IDS.UNSUBSCRIBE_INITIALIZE, "");
     }
 
+    public static dataVaultHandleOnScriptEvent(message: string): void {
+        this.getInstance().addonManager.dataVaultHandleOnScriptEvent(message);
+    }
+
+    public getDataVaultLastDataLoaded(): { data: string; count: number } {
+        return this.addonManager.getDataVaultLastDataLoaded();
+    }
+
     public static initSaveAddons(): void {
         this.getInstance().addonInitializer.saveAddons();
     }
